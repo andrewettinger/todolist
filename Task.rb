790c96e824185bff44016860b7010f1748a0c320
Andrew Ettinger <aettinger94@gmail.com>
2015-06-23 17:56:48 -0400

    2nd iteration: able to remove tasks

---
+++ Task.rb
@@ -0,0 +1,8 @@
+class Task
+	attr_accessor :content
+
+	def initialize(content)
+		@content = content
+	end
+
+end
--- ex.rb
+++ ex.rb
@@ -1,19 +1,37 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require_relative 'Task.rb'
 
 enable(:sessions)
+
+task_array = []
 
 get '/' do
 	erb(:todo)
 end
 
 post '/' do
-	@task = params[:task]
-	session[:task] = @task
+	task_array.push(Task.new(params[:task]))
 	redirect to('/list')
 end
 
 get '/list' do
-	"Your tasks are: #{session[:task]}"
+	@task_array = task_array
+	erb(:list)
 end
 
+get '/remove' do
+	@task_array = task_array
+	erb(:remove)
+end
+
+post '/remove' do
+	@task_array = task_array
+	remove = params[:remove]
+	@task_array.each do |remove_activity|
+		if remove_activity.content == remove
+		task_array.delete(remove_activity)
+	end
+end
+	redirect to '/list'
+end
